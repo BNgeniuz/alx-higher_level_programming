5c40e39f37bd8eeabc32ab13a00c7feada0cbbe6
Godwin Bruce <godwin.bruce@gmail.com>
2023-11-28 08:23:10 -0800

    asign random number to variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+last_num = abs(number) % 10
 if number < 0:
-    last_num = number % -10
-elif number >= 0:
-    last_num = number % 10
+    last_num = -(last_num)
+string = "Last digit of {} is {}".format(number, last_num)
     if last_num > 5:
-        print(f"Last digit of {number} is {last_num} and is greater than 5")
+        print(f"{string} and is greater than 5")
     elif last_num == 0:
-        print(f"Last digit of {number} is {last_num} and is 0")
-    else:
-        print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
+        print(f"{string} and is 0")
+    elif last_num < 6:
+        print(f"{string} and is less than 6 and not 0")
