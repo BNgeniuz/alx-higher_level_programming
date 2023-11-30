58badfa92b4a6c7152ece48983917c989d39bc46
Godwin Bruce <godwin.bruce@gmail.com>
2023-11-30 15:38:29 -0800

    program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    read = len(sys.argv) - 1
+    if read == 0:
+        print("0 arguments.")
+    elif read == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(read))
+        for j in range(read):
+            print("{}: {}".format(j + 1, sys.argc[j + 1]))
