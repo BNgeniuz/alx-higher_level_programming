8610a7ba75e3b3ed3c2225f133f26c99024a5d3e
Godwin Bruce <godwin.bruce@gmail.com>
2023-12-01 16:31:15 -0800

    program that imports all functions from the file calculator_1.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, div, mul
-import sys
 from sys import argv, exit
-
 if __name__ == "__main__":
 
     if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
 
         oprt = argv[2]
@@ -14,13 +12,15 @@ if __name__ == "__main__":
         b = int(argv[3])
 
         if oprt == '+':
-            print("{} + {}".format(a, b, add(a, b)))
-        elif oprt == '-':
-            print("{} - {}".format(a, b, sub(a, b)))
+            outcome = add(a, b)
+        elif oprt == "-":
+            outcome = sub(a, b)
+        elif oprt == "*":
+            outcome = mul(a, b)
         elif oprt == '/':
-            print("{} / {}".format(a, b, div(a, b)))
-        elif oprt == '*':
-            print("{} * { }".format(a, b, mul(a, b)))
+            outcome = div(a, b)
         else:
             print("Unknown operator: Available operators: +, -, *, and /")
             exit(1)
+
+            print("{} {} {} = {}".format(oprt, a, b, outcome))
