9356006c5a4bcae0e66f423f4cdaf11e029be2d4
Godwin Bruce <godwin.bruce@gmail.com>
2023-12-03 18:17:58 -0800

    program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
-
+    import hidden_4
     for j in dir(hidden_4):
-        if j[:2] != "__"
-        print(j)
+        if j[0] != '_' and j[1] != '_':
+            print(j)
