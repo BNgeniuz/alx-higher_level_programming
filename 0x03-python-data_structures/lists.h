0c5035484d179d243366e8cc901705be1fcae4d3
Godwin Bruce <godwin.bruce@gmail.com>
2023-12-04 16:16:55 -0800

    python data structure header files

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,25 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
-
 /**
- * struct listint_c - singly linked list
- * @m: integer
- * @ptr: points to the next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: singly lists
+ * Description: singly linked list node structure
+ * for project
  */
-typedef struct listint_c
+typedef struct listint_s
 {
-	int m;
-	struct listint_c *ptr;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *t);
-listint_t *add_nodeint_end(listint_t **tail, const int m);
-void free_listint(listint_t *tail);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-int is_palindrome(listint_t **tail);
+int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
