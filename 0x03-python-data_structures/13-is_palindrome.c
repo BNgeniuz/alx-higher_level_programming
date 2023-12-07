7162f334dade76c79405fc0f6ac89e83d6904251
Godwin Bruce <godwin.bruce@gmail.com>
2023-12-07 11:33:28 -0800

    linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,31 @@
 #include "lists.h"
 
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
-
 /**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- *
- * Return: A pointer to the head of the reversed list.
+ * aux_palind - program to check if palindrome is exist
+ * @head: palind input list
+ * @end: palind output list
  */
-listint_t *reverse_listint(listint_t **head)
+int aux_palind(listint_t **head, listint_t *end)
 {
-	listint_t *node = *head, *next, *prev = NULL;
-
-	while (node)
+	if (end == NULL)
+		return (1);
+	if (aux_palind(head, end->next) && (*head) ->n == end->n)
 	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
+		*head = (*head) ->next;
+		return (1);
 	}
-
-	*head = prev;
-	return (*head);
+	return (0);
 }
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
+ * palindrome - palind repetition or not
+ * @head: list input
  *
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
-int is_palindrome(listint_t **head)
+int is_palind(listint_t **head)
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
-
-	if (*head == NULL || (*head)->next == NULL)
+	if (head == NULL || *head == NULL)
 		return (1);
-
-	tmp = *head;
-	while (tmp)
-	{
-		size++;
-		tmp = tmp->next;
-	}
-
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
-
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);
-
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
-	{
-		if (tmp->n != rev->n)
-			return (0);
-		tmp = tmp->next;
-		rev = rev->next;
-	}
-	reverse_listint(&mid);
-
-	return (1);
+	return (aux_palind(head, *head));
 }
