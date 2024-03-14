1a1ab2f429ef53f78d26c7c342926cadd1fb4427
Godwin Bruce <godwin.bruce@gmail.com>
2024-03-13 22:48:42 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-/* script that imports an array
- * and computes a new array */
+/* script that imports an array */
 
 const list = require('./100-data').list;
 console.log(list);
