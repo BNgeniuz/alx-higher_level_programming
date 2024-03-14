06c53a5b53fabdc357da17c8284aba715c824509
Godwin Bruce <godwin.bruce@gmail.com>
2024-03-13 22:11:50 -0700

    corrections

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+
+
+/* script that prints 3 lines: (like 1-multi_languages.js */
+
 const lst = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const line of lst) {
   console.log(line);
