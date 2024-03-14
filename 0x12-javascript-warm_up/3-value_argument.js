3799057b33ca82a028540dcfe9b3cbc8443122b1
Godwin Bruce <godwin.bruce@gmail.com>
2024-03-13 19:20:33 -0700

    Write a script that prints the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
 /*  script that prints the first argument passed to it */
-const myArgs = process.argv.slice(2);
-if (!process.argv[2]) {
+
+const arg = process.argv;
+
+if (!arg[2]) {
   console.log('No argument');
 } else {
-  console.log(myArgs[0]);
+  console.log(arg[2]);
 }
