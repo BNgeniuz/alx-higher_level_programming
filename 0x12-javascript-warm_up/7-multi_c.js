031bafe3ff5afe485b55f7256f729d6c345ed4b7
Godwin Bruce <godwin.bruce@gmail.com>
2024-03-13 22:13:00 -0700

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+
+/* script that prints x times “C is fun” */
+
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
