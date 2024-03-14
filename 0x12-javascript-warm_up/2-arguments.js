0697a36b252c9cf76a0438b7af5f338e1d1b932e
Godwin Bruce <godwin.bruce@gmail.com>
2024-03-13 19:06:26 -0700

    Write a script that prints a message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 /* how to pass an arguments in java */
 const args = process.argv;
-if (argv.length === 2) {
+if (args.length === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
