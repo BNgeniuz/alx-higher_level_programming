35a79f6cf9e08820b4b7aff4a0476bc26c10f8c2
Godwin Bruce <godwin.bruce@gmail.com>
2024-04-23 09:04:36 -0700

    web scrapping java scrapping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,43 +1,28 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movie_id = process.argv[2]
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movie_id;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-  const filmData = JSON.parse(body);
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
 
-	const characterUrls = filmData.characters;
+    console.log(`Characters of "${movieData.title}":`);
 
-	characterUrls.forEach(characterUrl => {
-		request(characterUrl, (error, response, body) => {
-			if (error) {
-				console.error(error);
-				return;
-			}
+    movieData.characters.forEach((characterUrl) => {
+      request(characterUrl, function (charError, charResponse, charBody) {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
 
-			const characterData = JSON.parse(body);
-
-			console.log(characterData.name);
-		});
-	});
-  const characterUrls = filmData.characters;
-
-  characterUrls.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-
-      console.log(characterData.name);
+          console.log(characterData.name);
+        } else {
+          console.error('Error fetching character data:', charError);
+        }
+      });
     });
-  });
+  } else {
+    console.error('Error fetching movie data:', error);
+  }
 });
